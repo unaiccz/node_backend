0822b8c309b82ee58858ef774424da6e0d67d926
unai calderon <88508192+unaiccz@users.noreply.github.com>
2024-01-15 09:09:13 +0100

    Update Routes.js

--- routes/Routes.js
+++ routes/Routes.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import { CreateBlog, DeleteBlog, UpdateBlog, getAll, getBlog } from '../controllers/Blogcontroller.js';
 const router = express.Router();
+//Blog API
 router.get('/', getAll)
 router.get('/:id', getBlog)
 router.post('/', CreateBlog)
 router.put('/:id', UpdateBlog)
 router.delete('/:id', DeleteBlog)
 export default router;
+// Here other API enpoints/
